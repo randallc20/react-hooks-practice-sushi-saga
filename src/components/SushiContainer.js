e36fd5449d7a5caea8173854ab80c8b5f947e07c
chrisrandall <chris.f.randall@gmail.com>
2022-06-14 11:53:51 -0600

    done

--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi"
 
-function SushiContainer(props) {
+function SushiContainer({sushis, handleEatSushi}) {
+  const [sushiIndex, setSushiIndex] = useState(0);
+
+  const sushiComponents = sushis
+    .slice(sushiIndex, sushiIndex + 4)
+    .map((sushi) => (
+      <Sushi 
+      key={sushi.id} 
+      sushi={sushi} 
+      handleEatSushi={handleEatSushi}
+      />
+    ));
+
+  function handleClickMore() {
+    // (sushiIndex + 4) % sushis.length
+    // is a way to wrap back around to the beginning of the array once we get to the end
+    // using the remainder (%) operator
+    setSushiIndex((sushiIndex) => (sushiIndex + 4) % sushis.length);
+  }
+
+  //console.log(sushis)
+
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
-      <MoreButton />
+      {sushiComponents}
+      <MoreButton onClickMore={handleClickMore}/>
     </div>
   );
 }
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,6 +1,7 @@
 import React from "react";
+import Wallet from "./Wallet";
 
-function Table({ plates = [] }) {
+function Table({ wallet, onAddMoney, plates = [] }) {
   // renders an empty plate for every element in the array
   const emptyPlates = plates.map((_, index) => (
     <div key={index} className="empty-plate" style={{ top: -7 * index }} />
@@ -9,10 +10,11 @@ function Table({ plates = [] }) {
   return (
     <>
       <h1 className="remaining">
-        You have: ${/* Give me how much money I have left */} remaining!
+        You have: ${wallet} remaining!
       </h1>
       <div className="table">
         <div className="stack">{emptyPlates}</div>
+        <Wallet onAddMoney={onAddMoney}/>
       </div>
     </>
   );
